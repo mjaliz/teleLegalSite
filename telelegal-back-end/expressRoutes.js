94c5b9d1d1334f28b924567da114485c6f35e76e
mjaliz <mrph14@gmail.com>
2024-08-04 23:46:42 +0330

    creating jwt link

--- telelegal-back-end/expressRoutes.js
+++ telelegal-back-end/expressRoutes.js
@@ -1,6 +1,26 @@
 // This is where all our express stuff happens (routes)
 const app = require("./server").app;
+const jwt = require("jsonwebtoken");
+const linkSecret = "dsfjkslkdfjlwkej323423@#@Reskdfjw";
 
-app.get("/test", (req, res) => {
-  res.json("This is a test route");
+// This route is for US! In production, a receptionst, or calender/scheduling app
+// would send this out. We will print it out and paste it in. It will drop us
+// on our React site with the right info for CLIENT1 to make an offer
+app.get("/user-link", (req, res) => {
+  // Data for end-user's appt
+  const apptData = {
+    proffessionalsFullName: "MJ Alizadeh. E.N.",
+    apptDate: Date.now(),
+  };
+
+  // We need to encode this data to a token
+  const token = jwt.sign(apptData, linkSecret);
+  res.send("https://localhost:3000/join-video?token=" + token);
+  // res.json("This is a test route");
+});
+
+app.get("/validate-link", (req, res) => {
+  const token = req.query.token;
+  const decodedData = jwt.verify(token, linkSecret);
+  res.json(decodedData);
 });
