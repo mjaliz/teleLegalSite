dfd948cee13ad25d58703eeaa9825775211fced9
mjaliz <mrph14@gmail.com>
2024-08-09 00:03:24 +0330

    wireup redux

--- telelegal-front-end/src/index.js
+++ telelegal-front-end/src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createStore } from "redux"; // It's deprecated and the prefable way is to use redux toolkit in my app
+import { Provider } from "react-redux";
 import App from "./App";
+import rootReducer from "./redux-elements/reducers/rootReducer";
+
+const theStore = createStore(rootReducer);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  //<React.StrictMode>
-  <App />
-  //</React.StrictMode>
+  <Provider store={theStore}>
+    <App />
+  </Provider>
 );
---
+++ telelegal-front-end/src/redux-elements/reducers/rootReducer.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import callStatusReducer from "./callStatusReducer";
+
+const rootReducer = combineReducers({ callStatus: callStatusReducer });
+
+export default rootReducer;
